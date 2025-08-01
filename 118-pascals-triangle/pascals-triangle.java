373573fc784246e0200dadade050032f2f5fd192
Himabindu2006 <165761693+Himabindu2006@users.noreply.github.com>
2025-08-01 09:40:18 +0530

    Time: 1 ms (82.66%) | Memory: 41.9 MB (79.28%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            // Create a new row
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                // First and last elements are always 1
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    // Sum of the two numbers directly above
+                    int val = triangle.get(i - 1).get(j - 1) + triangle.get(i - 1).get(j);
+                    row.add(val);
+                }
+            }
+
+            // Add the row to the triangle
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+}
