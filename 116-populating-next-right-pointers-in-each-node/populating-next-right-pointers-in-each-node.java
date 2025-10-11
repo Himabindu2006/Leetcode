91c970a2eb5dfc3d8303c5c268582bac5a217225
Himabindu2006 <165761693+Himabindu2006@users.noreply.github.com>
2025-10-11 10:10:52 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (9.69%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null)
+            return null;
+
+        // If left child exists
+        if (root.left != null) {
+            // Connect left → right
+            root.left.next = root.right;
+
+            // Connect right → next.left (if next exists)
+            if (root.next != null)
+                root.right.next = root.next.left;
+
+            // Recurse for left and right subtrees
+            connect(root.left);
+            connect(root.right);
+        }
+
+        return root;
+    }
+}
+
